ea42dbf2584de8c7208d8340b66bc4d216da3397
Mateus Jesus <mateusjdev@gmail.com>
2022-11-04 14:04:14 -0300

    implementando a autenticação com o Google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,13 +1,19 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+// Garantindo o redirecionamento do navegador
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
-  // email: string;
   avatarUrl: string;
 }
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>; // processo de autenticação
 }
 
@@ -20,19 +26,46 @@ export const AuthContext = createContext({} as AuthContextDataProps)
 
 // Compartilhamento do contexto com toda a aplicação
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  // Começa como um objeto vazio, mas o tipo dele é o UserProps
+  const [user, setUser] = useState<UserProps>({} as UserProps)
 
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '942960564473-v85te5vflt1tftqmno9ik6ab5gsknsek.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('TOKEN DE AUTENTICAÇÃO ===>', access_token)
+  }
+  
   // Definição de função de signIn
   async function signIn() {
-    console.log('Vamos logar!')
+    try {
+      setIsUserLoading(true)
+      await promptAsync(); // inicia o fluxo de autenticação
+    } catch (err) {
+      console.error(err)
+      throw err;
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  // O useEffect é executado sempre que a resposta mudar
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
   
   return (
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: 'Mateus',
-        avatarUrl: 'https://github.com/MateusJSouza.png'
-      }
+      isUserLoading,
+      user,
     }}>
       {children}
     </AuthContext.Provider>
