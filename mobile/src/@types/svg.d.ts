c0f11f96efa0c0e8c317e89366d58b2b8f706eca
Mateus Jesus <mateusjdev@gmail.com>
2022-11-04 12:11:40 -0300

    feat: utilização de svg e tipagem

---
+++ mobile/src/@types/svg.d.ts
@@ -0,0 +1,7 @@
+// Arquivo de definição de tipagem quando for importado um arquivo SVG
+declare module "*.svg" {
+  import React from 'react';
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
