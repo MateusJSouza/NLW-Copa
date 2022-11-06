8e04498434de71d34efd829cb12292448387b9e5
Mateus Jesus <mateusjdev@gmail.com>
2022-11-05 22:56:29 -0300

    criação de rotas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,13 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-
-import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
-import { THEME } from './src/styles/theme';
 import { AuthContextProvider } from './src/contexts/AuthContext';
+
+import { Routes } from './src/routes';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -20,8 +21,8 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <Pools />  : <Loading />}
-      </AuthContextProvider>  
+        {fontsLoaded ? <Routes />  : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
